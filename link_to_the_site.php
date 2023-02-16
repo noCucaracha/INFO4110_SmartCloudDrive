947afda41f4daa86a0f0cfb2dd1f48c28f550e4b
noCucaracha <120060763+noCucaracha@users.noreply.github.com>
2023-02-16 13:36:59 -0800

    link to the project

---
+++ link_to_the_site.php
@@ -0,0 +1,12 @@
+<?php
+header("Location: http://15.223.56.22/IntegrationProject/mypathfinding.html");
+die();
+?>
+<header>
+
+<title>redirecting...</title>
+</header>
+
+<body>
+<h1>Redirecting</h1>
+</body>
